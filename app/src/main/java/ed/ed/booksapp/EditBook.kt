86758cb657ac8eb80c6894881fc80bf4e6d5daeb
GeraldineGalindoG <widni.galindo@ucb.edu.bo>
2021-05-05 20:35:14 -0400

    Added edit book and corrected some hardcoded strings

---
+++ app/src/main/java/ed/ed/booksapp/EditBook.kt
@@ -0,0 +1,11 @@
+package ed.ed.booksapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditBook : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_book)
+    }
+}
